4be1345dcc1b0c25169dda1d32b879ea3726c0cc
Yago Theodoro <freonzx@gmail.com>
2020-10-20 19:43:25 -0300

    feat: disable 15 minute say funtion

--- bot.js
+++ bot.js
@@ -1,15 +1,15 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
-const fs = require("fs");
-const embeds = require("./embeds.js");
+const fs = require('fs');
+const embeds = require('./embeds.js');
 client.commands = new Discord.Collection();
 
 //const config = JSON.parse(fs.readFileSync('./settings.json', 'utf-8'));
 fs.readdir('./commands/', (err, files) => {
   if (err) console.log(err);
 
-  let jsfile = files.filter(f => f.split('.').pop() == 'js')
-  if (jsfile.length <= 0){
+  let jsfile = files.filter((f) => f.split('.').pop() == 'js');
+  if (jsfile.length <= 0) {
     console.log("Didn't find commands to load.");
     return;
   }
@@ -19,10 +19,10 @@ fs.readdir('./commands/', (err, files) => {
     console.log(`[+] Command ${f} loaded.`);
 
     if (Array.isArray(props.help.name)) {
-      props.help.name.forEach( function(item){
+      props.help.name.forEach(function (item) {
         client.commands.set(item, props);
-      })
-    }else{
+      });
+    } else {
       client.commands.set(props.help.name, props);
     }
     //console.log(client.commands);
@@ -30,15 +30,15 @@ fs.readdir('./commands/', (err, files) => {
 });
 
 //const bot_controller = "133504218391642113";
-const prefix = ">";
+const prefix = '>';
 
 client.login(process.env.BOT_TOKEN);
 
-client.on('message', async message => {
+client.on('message', async (message) => {
   const member = message.member;
   const mess = message.content.toLowerCase();
   const msgcmd = message.content.split(' ')[0].slice(prefix.length);
-  const args = message.content.split(' ').slice(1).join(" ");
+  const args = message.content.split(' ').slice(1).join(' ');
 
   //Handling Private messages sent to the bot
   if (message.channel.type === 'dm') {
@@ -50,8 +50,12 @@ client.on('message', async message => {
   }
 
   //Logging
-  let messTime = message.createdAt.toString().replace('2018','').replace('GMT-0300 (Hora oficial do Brasil)','');
-  if (mess.startsWith(prefix)) console.log(`[*] ${messTime} ${message.author.username}: ${mess}`);
+  let messTime = message.createdAt
+    .toString()
+    .replace('2018', '')
+    .replace('GMT-0300 (Hora oficial do Brasil)', '');
+  if (mess.startsWith(prefix))
+    console.log(`[*] ${messTime} ${message.author.username}: ${mess}`);
   //Command Handler
   if (mess.startsWith(prefix)) {
     let commandfile = client.commands.get(msgcmd);
@@ -60,45 +64,46 @@ client.on('message', async message => {
   //Handles unknown commands
   else if (mess.startsWith(prefix)) {
     if (message.author.equals(client.user)) return;
-    message.reply("Ow chupa meu pal bro, todo mundo gosta de mim bro.");
+    message.reply('Ow chupa meu pal bro, todo mundo gosta de mim bro.');
   }
 });
 
 //On Ready
-client.on('ready', function() {
-  console.log("[*] Online.");
+client.on('ready', function () {
+  console.log('[*] Online.');
   client.user.setActivity('Skrrt skrrt skrrt skrrt', {
-    type: 'LISTENING'
+    type: 'LISTENING',
   });
 
   //setInterval(function(){ console.log(process.memoryUsage()); }, 600 * 1000);
 
-  client.guilds.forEach((guild) => { //for each guild the bot is in
-    let defaultChannel = "";
+  client.guilds.forEach((guild) => {
+    //for each guild the bot is in
+    let defaultChannel = '';
     guild.channels.forEach((channel) => {
-      if (channel.type == "text" && defaultChannel == "") {
-        if (channel.permissionsFor(guild.me).has("SEND_MESSAGES")) {
+      if (channel.type == 'text' && defaultChannel == '') {
+        if (channel.permissionsFor(guild.me).has('SEND_MESSAGES')) {
           defaultChannel = channel;
         }
       }
-    })
-    setInterval(function() {
-        var textArray = [
-          embeds.data.fazSol,
-          embeds.data.doubleCup,
-          "SKRRRRT SKRRRT SKRRRRRT <:swag:449814696544043009>",
-          embeds.data.todoMundo,
-          embeds.data.ferClothing,
-          embeds.data.kurtCobain,
-          "JOGUEI CODEINA NA MINHA FANTA <:swag:449814696544043009> <:lean:449823963762393089>",
-          "ELES ACHAM MINHA CALÇA APERTADA STYLE ELES SABEM SOU GANGSTA PARADISE <:swag:449814696544043009>",
-          "AY, AY, AY, AY, AY AY, AY, AY, AY, AY AY, AY, AY, AY, AY",
-          "AY AY AY SK CONTRATA O MARQUINHOS BRO"
-        ];
+    });
+    // setInterval(function() {
+    //     var textArray = [
+    //       embeds.data.fazSol,
+    //       embeds.data.doubleCup,
+    //       "SKRRRRT SKRRRT SKRRRRRT <:swag:449814696544043009>",
+    //       embeds.data.todoMundo,
+    //       embeds.data.ferClothing,
+    //       embeds.data.kurtCobain,
+    //       "JOGUEI CODEINA NA MINHA FANTA <:swag:449814696544043009> <:lean:449823963762393089>",
+    //       "ELES ACHAM MINHA CALÇA APERTADA STYLE ELES SABEM SOU GANGSTA PARADISE <:swag:449814696544043009>",
+    //       "AY, AY, AY, AY, AY AY, AY, AY, AY, AY AY, AY, AY, AY, AY",
+    //       "AY AY AY SK CONTRATA O MARQUINHOS BRO"
+    //     ];
 
-        var randomN = Math.floor(Math.random() * textArray.length);
-        defaultChannel.send(textArray[randomN]) //send it to whatever channel the bot has permissions to send on
-      },
-      900 * 1000);
-  })
+    //     var randomN = Math.floor(Math.random() * textArray.length);
+    //     defaultChannel.send(textArray[randomN]) //send it to whatever channel the bot has permissions to send on
+    //   },
+    //   900 * 1000);
+  });
 });
